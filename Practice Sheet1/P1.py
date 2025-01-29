a49dff31cee22279d1ab8a176c11aa7f0ae5a7f8
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-01-29 15:58:41 +0530

    Add files via upload

---
+++ Practice Sheet1/P1.py
@@ -0,0 +1,9 @@
+# WAP to print Twinkle Twinkle Little Star poem in python.
+
+poem = """
+    Twinkle, twinkle, little star!
+    How I wonder what you are,
+    Up above the world so high,
+    Like a diamond in the sky.
+"""
+print(poem)
