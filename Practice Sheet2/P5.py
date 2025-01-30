256b4a321d4e7dc59b24e89d39e990e76fd2b8a0
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-01-30 16:22:09 +0530

    Add files via upload

---
+++ Practice Sheet2/P5.py
@@ -0,0 +1,6 @@
+# WAP to find average of two numbers entered by the user.
+
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+avg = (num1 + num2) / 2
+print(f"Average of {num1} and {num2} is {avg}")
