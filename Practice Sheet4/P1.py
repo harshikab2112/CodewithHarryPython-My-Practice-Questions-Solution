856fe73c5b04f3768a2f64864ada76b58f1efe97
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-13 23:44:13 +0530

    Add files via upload

---
+++ Practice Sheet4/P1.py
@@ -0,0 +1,19 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+fruit = []
+f1 = input("Enter First fruit name: ")
+fruit.append(f1)
+f2 = input("Enter Second fruit name: ")
+fruit.append(f2)
+f3 = input("Enter Third fruit name: ")
+fruit.append(f3)
+f4 = input("Enter Fourth fruit name: ")
+fruit.append(f4)
+f5 = input("Enter Fifth fruit name: ")
+fruit.append(f5)
+f6 = input("Enter Sixth fruit name: ")
+fruit.append(f6)
+f7 = input("Enter Seventh fruit name: ")
+fruit.append(f7)
+
+print(fruit)
