20ea8b97756310c843b949e892c83458dfd14043
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-14 00:31:15 +0530

    Add files via upload

---
+++ Practice Sheet5/P1.py
@@ -0,0 +1,14 @@
+# Write a program to create a dictionary of Hindi words
+# with values as their English translation.
+# Provide user with an option to look it up!
+
+words = {
+    "dard": "pain",
+    "maza": "fun",
+    "ganda": "dirty",
+    "billi": "cat",
+    "achha": "good",
+}
+
+word = input("Enter word you want meaning of: ")
+print(words[word])
