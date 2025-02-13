c13217cd11d50ef43d24c2d3cc7fd8e803ca4dd7
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-14 00:33:19 +0530

    Add files via upload

---
+++ Practice Sheet5/P9.py
@@ -0,0 +1,8 @@
+# Can you change the values inside a list which is contained in set S?
+# S={8, 7, 12, "Harry", [1,2]}
+
+S = {8, 7, 12, "Harry", [1, 2]}
+print(S)
+
+# immutable sets and Unhashable list
+# sets in Python cannot contain mutable elements like lists.
