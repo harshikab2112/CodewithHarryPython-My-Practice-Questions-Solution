b8b134413ccb5eca2a6b656297ec4dd812972dd3
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-14 00:31:30 +0530

    Add files via upload

---
+++ Practice Sheet5/P2.py
@@ -0,0 +1,22 @@
+# Write a program to input eight numbers from the user
+# and display all the unique numbers (once).
+
+mySet = set()
+num1 = int(input("Enter a number: "))
+mySet.add(num1)
+num2 = int(input("Enter a number: "))
+mySet.add(num2)
+num3 = int(input("Enter a number: "))
+mySet.add(num3)
+num4 = int(input("Enter a number: "))
+mySet.add(num4)
+num5 = int(input("Enter a number: "))
+mySet.add(num5)
+num6 = int(input("Enter a number: "))
+mySet.add(num6)
+num7 = int(input("Enter a number: "))
+mySet.add(num7)
+num8 = int(input("Enter a number: "))
+mySet.add(num8)
+
+print(mySet)
