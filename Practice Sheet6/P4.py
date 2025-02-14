940152ed8d2fbf8814fd9b56b3d9b570468bf074
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-14 23:15:32 +0530

    Add files via upload

---
+++ Practice Sheet6/P4.py
@@ -0,0 +1,8 @@
+# Write a program to find whether a given username contains less than 10 characters or not.
+
+a = input("Enter a username: ")
+
+if len(a) < 10:
+    print("Less than 10 character")
+else:
+    print("Not less than 10 character")
