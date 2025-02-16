2e088eb31026e78d1b222fccf6c6716f2fff9db5
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-17 00:36:37 +0530

    Add files via upload

---
+++ Practice Sheet7/P9.py
@@ -0,0 +1,14 @@
+# Write a program to print the following star pattern.
+# ***
+# * *
+# *** for n=3
+
+n = 10
+
+for i in range(n):  # row
+    for j in range(n):  # column
+        if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
