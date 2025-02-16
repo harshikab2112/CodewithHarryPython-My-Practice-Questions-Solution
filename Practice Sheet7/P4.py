51b182730e2a8e43bdae332e67f0f06f8c1fcebc
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-17 00:36:17 +0530

    Add files via upload

---
+++ Practice Sheet7/P4.py
@@ -0,0 +1,13 @@
+# Write a program to find whether a given number is prime or not.
+
+num = int(input("Enter a number: "))
+
+if num < 2:
+    print(f"{num} is not a prime number.")
+else:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print(f"{num} is not a prime number.")
+            break
+    else:
+        print(f"{num} is a prime number.")
