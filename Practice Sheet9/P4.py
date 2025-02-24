f06bcd18bf18302b994fbc19cff0917007a13e56
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-02-24 23:43:51 +0530

    Add files via upload

---
+++ Practice Sheet9/P4.py
@@ -0,0 +1,14 @@
+# A file contains a word "Donkey" multiple times.
+# You need to write a program which replace this word with ##### by updating the same file.
+
+with open("P4.txt", "rt") as f:
+    data = f.read()
+
+data = data.replace("donkey", "#####")
+
+with open("P4.txt", "w") as f:
+    f.write(data)
+
+with open("P4.txt", "rt") as f:
+    data = f.read()
+    print(data)
