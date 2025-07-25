ade8fd1269c829e06a33064a6b8acb2efd8418e1
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-07-25 08:47:07 +0530

    Update P2.py

--- Practice Sheet6/P2.py
+++ Practice Sheet6/P2.py
@@ -1,14 +1,16 @@
-# Write a program to find out whether a student has passed or failed if it requires a total of 40% and at least 33% in each subject to pass.
+# Write a program to find out whether a student has passed or failed
+# if it requires a total of 40% and at least 33% in each subject to pass.
+
 # Assume 3 subjects and take marks as an input from the user.
 
 sub1 = float(input("Enter the 1st subject marks: "))
 sub2 = float(input("Enter the 2nd subject marks: "))
 sub3 = float(input("Enter the 3rd subject marks: "))
 
+# Since they are all out of 100.
 percent = (sub1 + sub2 + sub3) / 3
 
-if percent >= 40:
-    if sub1 >= 33 and sub2 >= 33 and sub3 >= 33:
-        print("Pass")
+if percent >= 40 and sub1 >= 33 and sub2 >= 33 and sub3 >= 33:
+    print("Pass")
 else:
     print("Fail")
